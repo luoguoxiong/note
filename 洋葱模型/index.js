b56f44c8df343990c877896425448b9235d19202
罗国雄 <1025558554@qq.com>
2020-10-20 19:49:56 +0800

    add

--- 洋葱模型/index.js
+++ 洋葱模型/index.js
@@ -1,3 +1,4 @@
+// 把一个函数组成嵌套成上一个函数的参数
 class App {
   constructor() {
     this.middleares = [];
@@ -5,7 +6,8 @@ class App {
   use(midd) {
     this.middleares.push(midd);
   }
-  compose() {
+
+  async run() {
     function createNext(mid, oldNext) {
       return async () => {
         await mid(oldNext);
@@ -14,15 +16,11 @@ class App {
     let next = () => {
       return Promise.resolve();
     };
-    return async () => {
-      for (let i = this.middleares.length - 1; i >= 0; i--) {
-        next = createNext(this.middleares[i], next);
-      }
-      await next();
-    };
-  }
-  run() {
-    this.compose()();
+
+    for (let i = this.middleares.length - 1; i >= 0; i--) {
+      next = createNext(this.middleares[i], next);
+    }
+    await next();
   }
 }
 const app = new App();
@@ -32,9 +30,32 @@ app.use(async (next) => {
   await next();
   console.log(4);
 });
-app.use(async (next) => {
-  console.log(2);
-  await next();
-  console.log(3);
-});
+// app.use(async (next) => {
+//   console.log(2);
+//   await next();
+//   console.log(3);
+// });
 app.run();
+
+function a(next) {
+  console.log('a');
+  next();
+  console.log('a1');
+}
+
+function b(next) {
+  console.log('b');
+  next();
+  console.log('b1');
+}
+function c(next) {
+  console.log('c');
+  next();
+  console.log('c1');
+}
+
+a(() => {
+  b(() => {
+    c(() => {});
+  });
+});
